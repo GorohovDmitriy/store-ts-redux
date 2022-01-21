7b071b3d7efbea011bdbbbe9e49a68b8ddd26f29
Gorohov <d.gorohov@itransition.com>
2022-01-21 16:27:10 +0300

    added photo to readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 - `run app - npm run json server and npm start`
+
+![screen](foto1.png)
+![screen](foto2.png)
+![screen](foto3.png)
--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const ProductPage = () => {
-  return <div>SELECTED ITEM</div>;
-};
+const ProductPage = () => <div>SELECTED ITEM</div>;
 
 export default ProductPage;
