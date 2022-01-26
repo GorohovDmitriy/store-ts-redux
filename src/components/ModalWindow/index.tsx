3591ed58ea3242a9e6677a247c8864425b650763
Gorohov <d.gorohov@itransition.com>
2022-01-26 16:26:19 +0300

    deleted comment

--- src/components/ModalWindow/index.tsx
+++ src/components/ModalWindow/index.tsx
@@ -59,40 +59,6 @@ const ModalWindow: FC<ModalProps> = ({
         </Button>
       </DialogActions>
     </Dialog>
-    // <Modal
-    //   open={open}
-    //   onClose={handleClose}
-    //   aria-labelledby="modal-modal-title"
-    //   aria-describedby="modal-modal-description"
-    // >
-    //   <Box sx={style}>
-    //     <Grid container spacing={2}>
-    //       <Grid item></Grid>
-    //       <Grid item xs={12} sm container>
-    //         <Grid item xs container direction="column" spacing={2}>
-    //           <Grid item xs>
-    //             <Typography gutterBottom variant="h6" component="h2">
-    //               Оформление заказа
-    //             </Typography>
-    //             <Typography variant="body2" gutterBottom>
-    //               {delcOfNumber(products.length)} {products.length}
-    //             </Typography>
-    //           </Grid>
-    //           <Grid item>
-    //             <Button onClick={handleClose} variant="contained">
-    //               Оформить
-    //             </Button>
-    //           </Grid>
-    //         </Grid>
-    //         <Grid item>
-    //           <Typography variant="subtitle1" component="div">
-    //             {totalPrice} BYN
-    //           </Typography>
-    //         </Grid>
-    //       </Grid>
-    //     </Grid>
-    //   </Box>
-    // </Modal>
   );
 };
 
