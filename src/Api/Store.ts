ef2af28ef32f869268c7592f6ca677e83a5821c0
Gorohov <d.gorohov@itransition.com>
2022-01-27 12:52:16 +0300

    api

--- src/Api/Store.ts
+++ src/Api/Store.ts
@@ -3,6 +3,6 @@ import axios, { AxiosResponse } from "axios";
 
 export default class Store {
   static async getProduct(): Promise<AxiosResponse<IProduct[]>> {
-    return axios.get<IProduct[]>(`https://localhost:3001/apple/`);
+    return axios.get<IProduct[]>(`https://store-ts-redux.herokuapp.com/apple/`);
   }
 }
